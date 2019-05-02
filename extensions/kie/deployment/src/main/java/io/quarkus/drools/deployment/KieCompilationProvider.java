bd576f81c1540bff34c93fb58cab4a10079a5e52
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-05-02 13:56:03 +0200

    workaround for separate compilations; requires JBPM-8458-input-datatypes to work

--- extensions/kie/deployment/src/main/java/io/quarkus/drools/deployment/KieCompilationProvider.java
+++ extensions/kie/deployment/src/main/java/io/quarkus/drools/deployment/KieCompilationProvider.java
@@ -15,6 +15,7 @@ import org.kie.submarine.codegen.GeneratedFile;
 import org.kie.submarine.codegen.process.ProcessCodegen;
 
 import io.quarkus.dev.JavaCompilationProvider;
+import io.quarkus.gizmo.ClassCreator;
 
 public class KieCompilationProvider extends JavaCompilationProvider {
 
@@ -30,16 +31,16 @@ public class KieCompilationProvider extends JavaCompilationProvider {
 
     @Override
     public void compile(Set<File> filesToCompile, Context context) {
-        String appPackageName = "org.kie";
+        String appPackageName = System.getProperty("kie.codegen.packageName", "org.kie");
         File outputDirectory = context.getOutputDirectory();
         try {
 
             ApplicationGenerator appGen = new ApplicationGenerator(appPackageName, outputDirectory)
                     .withDependencyInjection(true);
-            appGen.withGenerator(
-                    ProcessCodegen.ofFiles(new ArrayList<>(filesToCompile))); //fixme in submarine-codegen: should accept any collection
+            ProcessCodegen processCodegen = appGen.withGenerator(
+                    ProcessCodegen.ofFiles(new ArrayList<>(filesToCompile)));//fixme in submarine-codegen: should accept any collection
 
-            Collection<GeneratedFile> generatedFiles = appGen.generate();
+            Collection<GeneratedFile> generatedFiles = processCodegen.generate();
 
             HashSet<File> generatedSourceFiles = new HashSet<>();
             for (GeneratedFile file : generatedFiles) {
@@ -48,9 +49,46 @@ public class KieCompilationProvider extends JavaCompilationProvider {
                 generatedSourceFiles.add(path.toFile());
             }
             super.compile(generatedSourceFiles, context);
+            touchEmptyClassFiles(filesToCompile);
         } catch (IOException e) {
             throw new KieCompilerException(e);
         }
+    }
+
+    /*
+     *
+     * quarkus core expects compiled(foo/bar/baz/origin.ext) == foo/bar/baz/origin.class
+     * so we create one to make it happy
+     *
+     */
+    private void touchEmptyClassFiles(Set<File> filesToCompile) {
+        String SRCMAINJAVA = "src/main/java";
+
+        for (File file : filesToCompile) {
+            file.getParentFile().mkdirs();
+            String filePath = file.getPath();
+            int index = filePath.indexOf(SRCMAINJAVA) + SRCMAINJAVA.length() + 1;
+            String className = filePath.substring(index)
+                    .replace(handledExtension(), "");
+
+            String classFileName = filePath.replace(SRCMAINJAVA, "target/classes")
+                    .substring(0, 1 + filePath.length() - handledExtension().length()) + ".class";
+
+            writeEmptyClassFile(className, classFileName);
+        }
+    }
+
+    private void writeEmptyClassFile(String className, String classFileName) {
+        ClassCreator
+                .builder()
+                .className(className)
+                .classOutput((name, data) -> {
+                    try {
+                        Files.write(Paths.get(classFileName), data);
+                    } catch (IOException e) {
+                        throw new KieCompilerException(e);
+                    }
+                }).build().close();
     }
 
     private Path pathOf(String path, String relativePath) {
