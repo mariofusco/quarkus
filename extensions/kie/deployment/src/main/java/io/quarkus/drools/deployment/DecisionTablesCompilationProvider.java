40ff0938d4c31e051670a722aa0ba79b4bad8481
mariofusco <mario.fusco@gmail.com>
2019-05-21 14:15:44 +0200

    kie extension

---
+++ extensions/kie/deployment/src/main/java/io/quarkus/drools/deployment/DecisionTablesCompilationProvider.java
@@ -0,0 +1,26 @@
+package io.quarkus.drools.deployment;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import org.kie.api.io.ResourceType;
+import org.kie.submarine.codegen.ApplicationGenerator;
+import org.kie.submarine.codegen.Generator;
+import org.kie.submarine.codegen.rules.IncrementalRuleCodegen;
+
+public class DecisionTablesCompilationProvider extends KieCompilationProvider {
+
+    @Override
+    public Set<String> handledExtensions() {
+        return Collections.singleton(".xls");
+    }
+
+    @Override
+    protected Generator addGenerator(ApplicationGenerator appGen, Set<File> filesToCompile, Context context)
+            throws IOException {
+        return new IncrementalRuleCodegen(context.getOutputDirectory().toPath().getParent().getParent(), filesToCompile,
+                ResourceType.DTABLE);
+    }
+}
---
+++ extensions/kie/deployment/src/main/java/io/quarkus/drools/deployment/KieCompilationProvider.java
@@ -0,0 +1,52 @@
+package io.quarkus.drools.deployment;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.kie.submarine.codegen.ApplicationGenerator;
+import org.kie.submarine.codegen.GeneratedFile;
+import org.kie.submarine.codegen.Generator;
+
+import io.quarkus.dev.JavaCompilationProvider;
+
+public abstract class KieCompilationProvider extends JavaCompilationProvider {
+
+    @Override
+    public final void compile(Set<File> filesToCompile, Context context) {
+        String appPackageName = System.getProperty("kie.codegen.packageName", "org.kie");
+        File outputDirectory = context.getOutputDirectory();
+        try {
+
+            ApplicationGenerator appGen = new ApplicationGenerator(appPackageName, outputDirectory)
+                    .withDependencyInjection(true);
+            Generator generator = addGenerator(appGen, filesToCompile, context);
+
+            Collection<GeneratedFile> generatedFiles = generator.generate();
+
+            HashSet<File> generatedSourceFiles = new HashSet<>();
+            for (GeneratedFile file : generatedFiles) {
+                Path path = pathOf(outputDirectory.getPath(), file.relativePath());
+                Files.write(path, file.contents());
+                generatedSourceFiles.add(path.toFile());
+            }
+            super.compile(generatedSourceFiles, context);
+        } catch (IOException e) {
+            throw new KieCompilerException(e);
+        }
+    }
+
+    protected abstract Generator addGenerator(ApplicationGenerator appGen, Set<File> filesToCompile, Context context)
+            throws IOException;
+
+    private Path pathOf(String path, String relativePath) {
+        Path p = Paths.get(path, relativePath);
+        p.getParent().toFile().mkdirs();
+        return p;
+    }
+}
